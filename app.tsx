1565833c63c23f341ab99c8dddfe9137a0de04ad
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-04 11:45:22 -0500

    :art: Format

--- app.tsx
+++ app.tsx
@@ -13,7 +13,8 @@ const App = () => {
 
   return (
     <main
-      class={tw`h-screen bg-purple-400 flex items-center justify-center flex-col`}
+      class={tw
+        `h-screen bg-purple-400 flex items-center justify-center flex-col`}
     >
       <p class={tw`font-bold text(center 7xl gray-800)`}>
         {date.toLocaleTimeString()}
