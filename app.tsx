d7604b9cd7585cfd37d5852d9ad1477386784c5b
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-04 11:26:36 -0500

    :recycle: Move Vanilla CSS to twind

--- app.tsx
+++ app.tsx
@@ -1,5 +1,9 @@
-import React, { useState } from "https://esm.sh/react@17.0.2";
+import React from "https://esm.sh/react@17.0.2";
 import ReactDOM from "https://esm.sh/react-dom@17.0.2";
+
+import { h, Component, render, useState } from "https://esm.sh/preact";
+
+import { tw } from "https://esm.sh/twind";
 
 const App = () => {
   const [date, setDate] = useState(new Date());
@@ -8,7 +12,26 @@ const App = () => {
     setDate(new Date());
   }, 1000);
 
-  return <h1>{date.toLocaleTimeString()}</h1>;
+  return (
+    <main
+      className={tw`h-screen bg-purple-400 flex items-center justify-center flex-col`}
+    >
+      <p className={tw`font-bold text(center 5xl gray-800)`}>
+        The current time is {date.toLocaleTimeString()}.
+      </p>
+
+      <footer className={tw`text-center text-gray-500 mt-10`}>
+        <address>
+          <a
+            className={tw`text(gray-500 2xl) hover:text-blue-700`}
+            href="https://ultirequiem.com"
+          >
+            Eliaz Bobadilla
+          </a>
+        </address>
+      </footer>
+    </main>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
