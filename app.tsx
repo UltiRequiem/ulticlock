5619b7cf6d55f5e8156efd3e28100ce3de60192d
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-04-13 23:00:48 +0200

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,17 +1,33 @@
 /** @jsx h */
 
 import { h, render } from "https://esm.sh/preact";
-import { useEffect, useState } from "https://esm.sh/preact/hooks";
+import { useEffect, useState, useRef } from "https://esm.sh/preact/hooks";
 import { tw } from "https://esm.sh/twind";
 import { Footer } from "./components/Footer.tsx";
+
+function useInterval(callback: () => void, delay: number) {
+  const savedCallback = useRef();
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  });
+
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+
+    let id = setInterval(tick, delay);
+    return () => clearInterval(id);
+  }, [delay]);
+}
 
 const App = () => {
   const [date, setDate] = useState(new Date());
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
+  useInterval(() => {
+    setDate(new Date());
+  }, 1000);
 
     return () => clearInterval(interval);
   }, [date]);
